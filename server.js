524cb836cb99436e2eff4eaef5836ffca65014d5
sleeplesseditor <josh@bryson.me.uk>
2018-05-03 17:09:17 +0100

    Schema and GraphiQL setup

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+app.use('/', expressGraphQL({
+    schema,
+    graphiql: true
+}));
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
