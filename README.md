d18b469d8de36a9144d77f21def56c639896e999
sleeplesseditor <josh@bryson.me.uk>
2018-05-06 02:21:49 +0100

    nodemon and axios installed

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # GraphQLUsers 
 
 This is an early experiment in the use of GraphQL. It also uses Express.
+
+The app uses JSON server to server fake data.
--- schema/schema.js
+++ schema/schema.js
@@ -1,16 +1,11 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 const {
     GraphQLInt,
     GraphQLObjectType,
     GraphQLSchema,
     GraphQLString
 } = graphql;
-
-const users = [
-    { id: '23', firstName: 'Bill', age: 20 },
-    { id: '47', firstName: 'Sam', age: 21 }
-];
 
 const UserType = new GraphQLObjectType({
     name: 'User',
@@ -24,11 +19,12 @@ const UserType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-        user:{
+        user: {
             type: UserType,
             args: { id: { type: GraphQLString } },
             resolve(parentValue, args) {
-                return _.find(users, { id: args.id });
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                    .then(resp => resp.data);
             }
         }
     }
