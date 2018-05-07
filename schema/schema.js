8084a6083bd006cd81fea3c97fba7ceee0d6fbe3
sleeplesseditor <josh@bryson.me.uk>
2018-05-07 23:38:39 +0100

    Schema updated

--- schema/schema.js
+++ schema/schema.js
@@ -7,12 +7,24 @@ const {
     GraphQLString
 } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString }
+    }
+});
+
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
         id: { type: GraphQLString },
         firstName: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        age: { type: GraphQLInt },
+        company: {
+            type: CompanyType
+         }
     }
 });
 
